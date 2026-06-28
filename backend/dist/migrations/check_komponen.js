460fb9c81dc15410aecf19b959a568b8c80dfea2
Limzy71 <muhikhsanjr05@gmail.com>
2026-06-29 05:04:41 +0700

    chore: untrack build folders from git

--- backend/dist/migrations/check_komponen.js
+++
@@ -1,7 +0,0 @@
-import pool from '../config/database.js';
-async function listKomponen() {
-    const [rows] = await pool.query('SELECT kode_barang, nama_barang FROM inventory_stok WHERE tipe_item = "RM" OR kategori = "Komponen"');
-    console.log(JSON.stringify(rows, null, 2));
-    process.exit();
-}
-listKomponen();
